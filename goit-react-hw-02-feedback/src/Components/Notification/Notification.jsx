6c17db7b1d0b82579c5d6e5c19a1d27855bf8654
Ivan Berk <berkoivan@gmail.com>
2020-05-24 21:50:18 +0300

    fixed comments

--- goit-react-hw-02-feedback/src/Components/Notification/Notification.jsx
+++ goit-react-hw-02-feedback/src/Components/Notification/Notification.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Notification = ({ message = "No feedback given" }) => (
-  <>
-    <p>{message}</p>
-  </>
-);
+const Notification = ({ message = "No feedback given" }) => <p>{message}</p>;
 
 Notification.propTypes = {
   message: PropTypes.string,
